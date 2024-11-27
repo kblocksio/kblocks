5e7bb886a5311a9c8a46d6ffd6ee38cb19c00f73
Elad Cohen <eladc@monada.co>
2024-11-27 19:20:41 +0200

    chore: add endpoints log

--- packages/@kblocks/common/src/endpoints.ts
+++ packages/@kblocks/common/src/endpoints.ts
@@ -1,6 +1,11 @@
 export const getEndpoints = () => {
   const KBLOCKS_STORAGE_PREFIX = process.env.KBLOCKS_STORAGE_PREFIX ?? "";
   const KBLOCKS_API_KEY = process.env.KBLOCKS_API_KEY;
+  const host = process.env.KBLOCKS_HOST ?? "redis.staging.kblocks.io";
+  const port = process.env.KBLOCKS_PORT ? parseInt(process.env.KBLOCKS_PORT) : 18284;
+  const events = process.env.KBLOCKS_EVENTS_CHANNEL ?? `${KBLOCKS_STORAGE_PREFIX}kblocks-events`;
+  const control = process.env.KBLOCKS_CONTROL_CHANNEL ?? `${KBLOCKS_STORAGE_PREFIX}kblocks-control`;
+  console.log("endpoints", { host, port, events, control });
 
   return {
     redis: () => {
@@ -9,15 +14,15 @@ export const getEndpoints = () => {
       }
 
       return {
-        host: process.env.KBLOCKS_HOST ?? "redis.staging.kblocks.io",
-        port: process.env.KBLOCKS_PORT ? parseInt(process.env.KBLOCKS_PORT) : 18284,
+        host,
+        port,
         password: KBLOCKS_API_KEY,
         // username: process.env.KBLOCKS_SYSTEM_ID,
       };
     },
     channels: {
-      events: process.env.KBLOCKS_EVENTS_CHANNEL ?? `${KBLOCKS_STORAGE_PREFIX}kblocks-events`,
-      control: process.env.KBLOCKS_CONTROL_CHANNEL ?? `${KBLOCKS_STORAGE_PREFIX}kblocks-control`,
+      events,
+      control,
     }
   }
 };
